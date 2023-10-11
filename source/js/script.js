bad94f11e9b330b330a3d33bbbc7dd38803173dc
mgDentist <gmi1987@gmail.com>
2023-10-11 17:30:16 +0400

    Добавляет мобильный хедер + выпадающее меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,13 @@
 /* в этот файл добавляет скрипты*/
+const openMenuButton = document.querySelector('.js-toggle-button');
+const navMenu = document.querySelector('.header__nav');
+
+openMenuButton.addEventListener('click', () => {
+  if (navMenu.classList.contains('header__nav--closed')) {
+    navMenu.classList.remove('header__nav--closed');
+    openMenuButton.style.backgroundImage = 'url("/images/burger-close.svg")';
+  } else {
+    navMenu.classList.add('header__nav--closed');
+    openMenuButton.style.backgroundImage = 'url("/images/burger.svg")';
+  }
+});
