d43912ad1aca4c33c24838a007b1075303c2ad87
mgDentist <gmi1987@gmail.com>
2023-10-12 08:50:23 +0400

    добавляет правки в блок header

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,17 @@
 /* в этот файл добавляет скрипты*/
-const openMenuButton = document.querySelector('.js-toggle-button');
+// модальное окно
+const menuButton = document.querySelector('.js-toggle-button');
 const navMenu = document.querySelector('.header__nav');
 
-openMenuButton.addEventListener('click', () => {
+menuButton.addEventListener('click', () => {
   if (navMenu.classList.contains('header__nav--closed')) {
     navMenu.classList.remove('header__nav--closed');
-    openMenuButton.style.backgroundImage = 'url("/images/burger-close.svg")';
+    menuButton.style.backgroundImage = 'url("/images/burger-close.svg")';
   } else {
     navMenu.classList.add('header__nav--closed');
-    openMenuButton.style.backgroundImage = 'url("/images/burger.svg")';
+    menuButton.style.backgroundImage = 'url("/images/burger.svg")';
   }
 });
+
+// показ активной ссылки
+const links = 
