9087442b7a807b4805c55c86d9593baad06537d3
mgDentist <gmi1987@gmail.com>
2023-10-13 15:30:02 +0400

    добавляет скрип для работы слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -27,3 +27,99 @@ links.forEach((link) => {
   });
 });
 
+// слайдер
+const moveSlides = () => {
+  const slider = document.querySelector('.slider');
+  const slides = slider.querySelectorAll('.slider__slide');
+  const prevButton = slider.querySelector('.slider__button-prev');
+  const nextButton = slider.querySelector('.slider__button-next');
+  const paginationButtons = slider.querySelectorAll('.slider__button-pagination');
+  const svgImages = slider.querySelectorAll('.slider__pagination-button');
+
+  let currentSlideIndex = 0;
+
+  // Функция, которая скрывает все слайды и показывает только текущий слайд.
+  const showSlide = (index) => {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.classList.remove('slider__slide--hidden');
+      } else {
+        slide.classList.add('slider__slide--hidden');
+      }
+    });
+  };
+
+  //пагинация
+  const changePaginationColor = () => {
+    paginationButtons.forEach((paginationButton, i) => {
+      if (i === currentSlideIndex) {
+        svgImages[i].style.fill = '#7859CF';
+      } else {
+        svgImages[i].style.fill = '#ffffff';
+      }
+    });
+  };
+
+  // Показать первый слайд при загрузке страницы.
+  showSlide(currentSlideIndex);
+  // Показать первую пагинацию при загрузке страницы.
+  changePaginationColor();
+  //
+  if (currentSlideIndex === 0) {
+    prevButton.setAttribute('disabled', true);
+  }
+
+  // Обработчик для кнопки "Следующий слайд".
+  nextButton.addEventListener('click', () => {
+    if (currentSlideIndex < slides.length - 1) {
+      currentSlideIndex = currentSlideIndex + 1;
+      showSlide(currentSlideIndex);
+      changePaginationColor();
+    }
+    if (currentSlideIndex === slides.length - 1) {
+      nextButton.setAttribute('disabled', true);
+    }
+
+    prevButton.removeAttribute('disabled');
+
+  });
+
+  // Обработчик для кнопки "Предыдущий слайд".
+  prevButton.addEventListener('click', () => {
+    if (currentSlideIndex > 0) {
+      currentSlideIndex = currentSlideIndex - 1;
+      showSlide(currentSlideIndex);
+      changePaginationColor();
+    }
+    if (currentSlideIndex === 0) {
+      prevButton.setAttribute('disabled', true);
+    }
+
+    nextButton.removeAttribute('disabled');
+  });
+
+  // Обработчик для пагинации.
+  paginationButtons.forEach((paginationButton, i) => {
+    paginationButton.addEventListener('click', () => {
+      currentSlideIndex = i;
+      showSlide(currentSlideIndex);
+      changePaginationColor();
+
+      if (currentSlideIndex === 0) {
+        prevButton.setAttribute('disabled', true);
+      } else {
+        prevButton.removeAttribute('disabled');
+      }
+
+      if (currentSlideIndex === slides.length - 1) {
+        nextButton.setAttribute('disabled', true);
+      } else {
+        nextButton.removeAttribute('disabled');
+      }
+
+    });
+  });
+};
+
+moveSlides();
+
