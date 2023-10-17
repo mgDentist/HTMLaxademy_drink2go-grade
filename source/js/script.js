6fbd385bb62f949b844971eed43e8031fd746def
mgDentist <gmi1987@gmail.com>
2023-10-17 10:37:13 +0400

    Улучшает код для модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,21 @@
 /* в этот файл добавляет скрипты*/
 // модальное окно
-const menuButton = document.querySelector('.js-toggle-button');
-const navMenu = document.querySelector('.header__nav');
+const showAndHideModal = () => {
+  const menuButton = document.querySelector('.js-toggle-button');
+  const navMenu = document.querySelector('.header__nav');
 
-menuButton.addEventListener('click', () => {
-  if (navMenu.classList.contains('header__nav--closed')) {
-    navMenu.classList.remove('header__nav--closed');
-    menuButton.style.backgroundImage = 'url("/images/burger-close.svg")';
-  } else {
-    navMenu.classList.add('header__nav--closed');
-    menuButton.style.backgroundImage = 'url("/images/burger.svg")';
-  }
-});
+  menuButton.addEventListener('click', () => {
+    if (navMenu.classList.contains('header__nav--closed')) {
+      navMenu.classList.remove('header__nav--closed');
+      menuButton.style.backgroundImage = 'url("/images/burger-close.svg")';
+    } else {
+      navMenu.classList.add('header__nav--closed');
+      menuButton.style.backgroundImage = 'url("/images/burger.svg")';
+    }
+  });
+};
+
+showAndHideModal();
 
 // показ активной ссылки
 const showActiveLinkNav = () => {
